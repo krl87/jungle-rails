afd9af91558230709b149d87ea339bc92288faa0
Kayley <kayley.luftig@gmail.com>
2019-06-14 18:51:40 +0000

    :sparkles: add functionality between view/model and controller for categories on admin side

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -12,9 +12,17 @@ class Admin::CategoriesController < ApplicationController
     @category = Category.new(category_params)
 
     if @category.save
-      redrect_to [:admin, :categories], notice: 'Category created!'
+      redirect_to [:admin, :categories], notice: 'Category created!'
     else
       render :new
     end
+  end
+
+   private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
   end
 end
