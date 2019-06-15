dd827ccb86088bacc3ff663e0c39630fba4f769e
Kayley <kayley.luftig@gmail.com>
2019-06-15 18:54:42 +0000

    :construction: add routes for user registration

--- config/routes.rb
+++ config/routes.rb
@@ -16,8 +16,10 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:edit, :update, :show, :destroy]
-
   end
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
