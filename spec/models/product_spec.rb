b978ec7932264cc65304b084b17e71fe13bb7cfa
Kayley <kayley.luftig@gmail.com>
2019-06-18 00:24:00 +0000

    :white_check_mark: complete testing for products and categories

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Product, type :model do
     it 'not valid without a name' do
       category = Category.new(name: 'stickers')
       product = category.products.new(
+        name: nil,
         price: 100,
         quantity: 20
       )
@@ -14,16 +15,27 @@ RSpec.describe Product, type :model do
       category = Category.new(name: 'stickers')
       product = category.products.new(
         name: 'corgi',
+        price: nil,
         quantity: 20
       )
       expect(product).to_not be_valid
     end
 
-     it 'not valid without a quantity' do
+    it 'not valid without a quantity' do
       category = Category.new(name: 'stickers')
       product = category.products.new(
         name: 'corgi',
-        price: 20
+        price: 20,
+        quantity: nil
+      )
+      expect(product).to_not be_valid
+    end
+
+    it 'not valid without a category' do
+      product = category.products.new(
+        name: 'corgi',
+        price: 200,
+        quantity: 30
       )
       expect(product).to_not be_valid
     end
